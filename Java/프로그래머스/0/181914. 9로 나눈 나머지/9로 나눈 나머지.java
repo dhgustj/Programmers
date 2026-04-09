9e537571905cd79ca9a988dda4954c3b47cf58de
오현서 <ohs070611@naver.com>
2026-04-09 14:07:53 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 18.28 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        int sum = 0;
+        for(char c : number.toCharArray()){
+            sum += Character.getNumericValue(c);
+        }
+        answer = sum % 9;
+        return answer;
+    }
+}
